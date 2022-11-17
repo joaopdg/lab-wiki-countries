4567c950e7442013d884688324f7988001162330
joaopdg <jpgon@live.com.pt>
2022-11-17 14:38:05 +0000

    lab review for class

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -16,25 +16,40 @@ function CountryDetails() {
   }, [countryId]);
 
   return (
-    <div>
+    <>
       {oneCountry && (
-        <div className='myCard'>
+        <div className="myCard">
           <img
             src={`https://flagpedia.net/data/flags/icon/72x54/${oneCountry.alpha2Code.toLowerCase()}.png`}
             alt={oneCountry.name.common}
           />
           <h2>{oneCountry.name.common}</h2>
+
+          <div className="divider" />
+
           <p>Capital: {oneCountry.capital}</p>
           <p>Area: {oneCountry.area}</p>
-         
-          <p>Borders:</p>
-          {oneCountry.borders.map((el) => {
-            return  <Link to={`/${el}`}><p>{el}</p></Link>;
-          })}
-  
+
+          <div className="divider" />
+
+          <div className="cRow">
+            <div className="bordersM">
+              <p>Borders:</p>
+            </div>
+
+            <div>
+              {oneCountry.borders.map((el) => {
+                return (
+                  <Link to={`/${el}`}>
+                    <p>{el}</p>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
